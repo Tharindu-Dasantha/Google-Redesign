2c1b8ff07f849ae70eb544bcab3a8a5f0da0843b
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-06-19 14:19:39 +0530

    created the nav bar
    need to implement the burger menu
    need to implement the profile popup
    need to implement the profile pic
    basic structure is created

--- Main/script.js
+++ Main/script.js
@@ -12,3 +12,14 @@ burgerMenu.addEventListener('mouseenter', () => {
 burgerMenu.addEventListener('mouseleave', () => {
     burgerMenu.classList.remove('active');  
 })
+
+// Adding letter or a profile pic to the profile icon
+// later add the function to add an image as the profile picture
+const profileElement = document.querySelector('.shortcutItems_navbar_profile');
+const profileInitials = document.querySelector('.profile-initials');
+
+// set the initials as the profile pic
+let initials = "TD";
+profileInitials.textContent = initials;
+
+
